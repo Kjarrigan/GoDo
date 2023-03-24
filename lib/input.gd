17022d94e562c0d350bf3d287faff8e38b18d9d1
Kjarrigan <github@kjarrigan.de>
2023-03-25 00:00:04 +0100

    don't add empty tasks

--- lib/input.gd
+++ lib/input.gd
@@ -4,5 +4,6 @@ func _ready():
 	tree_exited.connect(func(): Globals.item_changed.emit())
 
 func _on_text_submitted(new_text):
-	Globals.add_task(get_meta("parent"), new_text)
+	if new_text != "":
+		Globals.add_task(get_meta("parent"), new_text)
 	queue_free()
