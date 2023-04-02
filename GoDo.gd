f83f365cdf2fc683bb607915eb5c39d672357860
Kjarrigan <github@kjarrigan.de>
2023-04-02 20:11:15 +0200

    Add scrollbox to list

--- GoDo.gd
+++ GoDo.gd
@@ -3,7 +3,7 @@ extends Panel
 @onready var input = preload("res://lib/input.tscn")
 
 func _ready():
-	Globals.root_list = $Border/List
-	Globals.new_input($Border/List)
+	Globals.root_list = %List
+	Globals.new_input(%List)
 	Globals.load_tasks()
 	Globals.item_selected.emit(null)
